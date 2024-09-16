ff6d4f720ac529da9a188db582506b1ae126b161
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-09-16 15:23:12 +0300

    created ad-block component

---
+++ src/components/ad-block/AdBlock.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./AdBlock.scss";
+
+export default function AdBlock() {
+    return (
+        <div className="ad-block container">
+            <h3 className="ad-block--title">РЕКЛАМНЫЙ БЛОК</h3>
+        </div>
+    );
+}
