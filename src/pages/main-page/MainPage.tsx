8589007af6fe01cf9ca70cd23fa2036bb66e3d27
Volodimirus <vladimir.eckimtsov@gmail.com>
2024-09-17 09:37:10 +0300

    added catalog component to main page

--- src/pages/main-page/MainPage.tsx
+++ src/pages/main-page/MainPage.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import AppHeader from "../../components/app-header/AppHeader";
 import AppNavigation from "../../components/app-navigation/AppNavigation";
 import AdBlock from "../../components/ad-block/AdBlock";
+import Catalog from "../../components/catalog/Catalog";
 
 import { ThemeContext, themes } from "../../contexts/ThemeContext";
 import ContactsAndSales from "../../components/contacts-and-sales/ContactsAndSales";
@@ -41,6 +42,15 @@ export default function MainPage() {
                             }}
                         />
                         <AdBlock />
+                        <Catalog
+                            isDark={theme === themes.dark}
+                            onChange={() => {
+                                if (theme === themes.light)
+                                    setTheme(themes.dark);
+                                if (theme === themes.dark)
+                                    setTheme(themes.light);
+                            }}
+                        />
                     </>
                 );
             }}
